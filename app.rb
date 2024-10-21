bcdb6c66a9b91f703d8ba35cabc38c831d22661b
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-21 15:13:24 +0000

    roots done

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
 end
+
+get("/square/new") do
+ erb(:new_square)
+end
+
+get("/square/results") do
+  @the_num= params.fetch("num_input").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+ end
+
+ get("/square_root/new") do
+  erb(:new_root)
+ end
+ 
+ get("/root/results") do
+   @the_root= params.fetch("root_input").to_f
+   @root_result = @the_root ** 0.5
+   erb(:root_result)
+  end
+ 
