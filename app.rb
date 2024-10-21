b6319d9c4d42f7abd11ad10b1ee65c0ca1a201cd
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-21 15:31:06 +0000

    more

--- app.rb
+++ app.rb
@@ -23,4 +23,18 @@ get("/square/results") do
    @root_result = @the_root ** 0.5
    erb(:root_result)
   end
+
+  get("/payment/new") do
+    erb(:new_payment)
+   end
+   
+   get("/payment/results") do
+     @the_apr = (params.fetch("apr_input").to_f)
+     @the_apr_unit = (params.fetch("apr_input").to_f)/100/12
+     @the_year = params.fetch("years_input").to_i 
+     @the_year_unit = params.fetch("years_input").to_i * 12
+     @the_principal = params.fetch("principal_input").to_f
+     @payment_result = (@the_apr_unit*@the_principal) / (1-(1+@the_apr_unit)**(@the_year_unit*-1))
+    erb(:payment_results)
+    end
  
